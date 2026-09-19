95d470417be493f29e5dc5f84f8276249ffa0e80
astronaots <sujarmirizkiyah@gmail.com>
2026-09-20 01:09:28 +0700

    Labwork 01 - Prelab

--- src/lw01/prelab/src/Main.java
+++ src/lw01/prelab/src/Main.java
@@ -1,28 +1,35 @@
+import java.io.InputStream;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-         Scanner sc = new Scanner(Main.class.getResourceAsStream("jobs.txt"));
+        InputStream is = Main.class.getResourceAsStream("jobs.txt");
+        if (is == null) {
+            System.err.println("File 'jobs.txt' not found in classpath.");
+            return;
+        }
 
-         List<PrintJob> listPrint = new ArrayList<>();
+        List<PrintJob> listPrint = new ArrayList<>();
 
-         int n = 5;
+        int n = 5;
+        try (Scanner sc = new Scanner(is)) {
 
-         while (n-- > 0) {
-            String line = sc.nextLine();
-            String[] dataPrint = line.split(" ");
+            while (n-- > 0) {
+                String jenis = sc.next();
+                String id = sc.next();
+                int jumlahPages = sc.nextInt();
 
-            if (dataPrint[0].equals("MONO")) {
-                PrintJob mono = new MonoPrint(dataPrint[1], Integer.parseInt(dataPrint[2]));
-                listPrint.add(mono);
-            } else {
-                PrintJob colour = new ColourPrint(dataPrint[1], Integer.parseInt(dataPrint[2]));
-                listPrint.add(colour);
+                if (jenis.equals("MONO")) {
+                    PrintJob mono = new MonoPrint(id, jumlahPages);
+                    listPrint.add(mono);
+                } else {
+                    PrintJob colour = new ColourPrint(id, jumlahPages);
+                    listPrint.add(colour);
+                }
             }
-         }
-        
-         for (PrintJob print : listPrint) {
-             System.out.println(print.summary());
-         }
+        }
+        for (PrintJob print : listPrint) {
+            System.out.println(print.summary());
+        }
     }
 }
